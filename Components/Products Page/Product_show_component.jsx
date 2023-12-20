0f48ece4d9fafdd6ab445270b15d673d4d192265
hassaammgl <h455a4mmehtab@gmail.com>
2023-12-20 22:54:48 +0500

    adsf

--- Components/Products Page/Product_show_component.jsx
+++ Components/Products Page/Product_show_component.jsx
@@ -17,9 +17,9 @@ import {
 } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import style from "@/styles/productDetails.module.scss";
-import 'react-inner-image-zoom/lib/InnerImageZoom/styles.css';
-import 'react-inner-image-zoom/lib/InnerImageZoom/styles.min.css';
-import InnerImageZoom from 'react-inner-image-zoom';
+import "react-inner-image-zoom/lib/InnerImageZoom/styles.css";
+import "react-inner-image-zoom/lib/InnerImageZoom/styles.min.css";
+import InnerImageZoom from "react-inner-image-zoom";
 import { FaChevronDown } from "react-icons/fa";
 import { BsCart3 } from "react-icons/bs";
 import { products } from "@/Components/productsArray";
@@ -39,7 +39,12 @@ const Product_show_component = ({ params }) => {
           <FaChevronDown />
         </div>
         <div className={style.body}>
-       <InnerImageZoom src={productDetail.imageLink} zoomSrc={productDetail.largeImageLink} />
+          <div className={style.left}>
+            <InnerImageZoom
+              src={productDetail.imageLink}
+              zoomSrc={productDetail.largeImageLink}
+            />
+          </div>
           <div className={style.right}>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
